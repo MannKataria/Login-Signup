1629813fc54e4796c87692e7ebe7c5ec0742a9ef
Mann <mannkataria15@gmail.com>
2023-09-25 16:27:01 +0530

    updated login component

--- src/Components/LoginSignup/Login.jsx
+++ src/Components/LoginSignup/Login.jsx
@@ -1,12 +1,62 @@
-import './Login.css'
-import tour_set from '../Assets/tour-set.png'
+import "./Login.css";
+import tour_set from "../Assets/tour_set_login.svg";
+import logo from "../Assets/logo.svg";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
-const Login = () =>{
-    return (
-        <div>
-            <h1>Login</h1>
+const Login = () => {
+  return (
+    <>
+      <div className="mainCont">
+        <div className="loginCont">
+          <form method="post" className="loginForm">
+            <div className="header">
+              <div className="text">Login</div>
+              <p className="normaltext">Enter your Account Details</p>
+            </div>
+
+            <div className="inputs">
+              <div className="input">
+                <input type="email" placeholder="Email Address" />
+              </div>
+              <div className="input">
+                <input type="password" placeholder="Password" />
+              </div>
+            </div>
+
+            <a href="/">Forget Password?</a>
+            <button className="loginSubmit" type="submit">
+              Login
+            </button>
+          </form>
+
+          <p className="normaltext" id="signupAcc">
+            Don't have an account?{" "}
+            <a href="/" className="signup">
+              Sign Up
+            </a>
+          </p>
         </div>
-    );
-}
 
-export default Login
+        <div className="logoCont">
+          <div>
+            <img src={logo} alt="" />
+            <h3 className="text">Sign in to unlock</h3>
+            <p className="titletext">best of Routier</p>
+          </div>
+          <img src={tour_set} alt="" className="tourSet" />
+        </div>
+
+        {/* for mobile view */}
+        <div className="shortlogoCont">
+          <div>
+            <img src={logo} alt="" />
+            <h3 className="text">Login to experience the</h3>
+            <p className="titletext">best of Routier</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Login;
--- src/Components/LoginSignup/Signup.jsx
+++ src/Components/LoginSignup/Signup.jsx
@@ -1,40 +1,44 @@
-import './Signup.css'
-import tour_set from '../Assets/tour-set.png'
+import "./Signup.css";
+import tour_set from "../Assets/tour-set-signup.svg";
 
 const Signup = (props) => {
-    return (
-        <>
-        {/* <img className="tourSet" src={tour_set} alt="tour-set" /> */}
-        <div className="container">
-            <form method="post" className='signupForm'>
-                <div className="header">
-                    <div className="text">Create Account</div>
-                </div>
-                <div className="inputs">
-                    <div className="input">
-                        <input type="text" placeholder='Full Name'/>
-                    </div>
-                    <div className="input">
-                        <input type="email" placeholder='Email Address'/>
-                    </div>
-                    <div className="input">
-                        <input type="password" placeholder='Password'/>
-                    </div>
-                    <div className="input">
-                        <input type="password" placeholder='Repeat Password'/>
-                    </div>
-                </div>
-                <div className="submit-container">
-                    <button className='signupSubmit' type='submit'>Create Account</button>
-                </div>
-            </form>
-                <div className="form-switch">
-                    Already have an account?
-                    <button className='form-switch-btn' onClick={props.xyz("login")}>Log In</button>
-                </div>
+  return (
+    <>
+      {/* <img className="tourSet" src={tour_set} alt="tour-set" /> */}
+      <div className="container">
+        <form method="post" className="signupForm">
+          <div className="header">
+            <div className="text">Create Account</div>
+          </div>
+          <div className="inputs">
+            <div className="input">
+              <input type="text" placeholder="Full Name" />
+            </div>
+            <div className="input">
+              <input type="email" placeholder="Email Address" />
+            </div>
+            <div className="input">
+              <input type="password" placeholder="Password" />
+            </div>
+            <div className="input">
+              <input type="password" placeholder="Repeat Password" />
+            </div>
+          </div>
+          <div className="submit-container">
+            <button className="signupSubmit" type="submit">
+              Create Account
+            </button>
+          </div>
+        </form>
+        <div className="form-switch">
+          Already have an account?
+          <button className="form-switch-btn" onClick={props.xyz("login")}>
+            Log In
+          </button>
         </div>
-        </>
-    )
-}
+      </div>
+    </>
+  );
+};
 
-export default Signup
+export default Signup;
