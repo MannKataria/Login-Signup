85c94d6fda7ac4c6b88122b7e04776d2f9aa0f86
Shwet <shwetpatel2712@gmail.com>
2023-09-25 14:02:59 +0530

    added signup with google/fb and picture

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import logo from "./logo.svg";
 import "./App.css";
-import LoginSignup from "./Components/LoginSignup/Signup";
+import Signup from "./Components/LoginSignup/Signup";
 import Login from "./Components/LoginSignup/Login";
 import { useState } from "react";
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 function App() {
-  const [currPage, setCurrPage] = useState("login");
+  const [currPage, setCurrPage] = useState("Signup");
   return (
+    <GoogleOAuthProvider clientId="169559215961-i0o7klhc5rhvsm81nctt95v7drpnenjj.apps.googleusercontent.com">
     <div className="App">
       {currPage == "login" ? <Login /> : <Signup />}
     </div>
+    </GoogleOAuthProvider>
   );
 }
 
--- src/Components/LoginSignup/Signup.jsx
+++ src/Components/LoginSignup/Signup.jsx
@@ -1,34 +1,93 @@
 import './Signup.css'
-import tour_set from '../Assets/tour-set.png'
+import { useState } from 'react';
+import tourset from '../Assets/tourSet.png'
+import fb from '../Assets/fb.png'
+import logo from '../Assets/Signuplogo.png'
+import googlebutton from '../Assets/google.png'
+import jwt_decode from 'jwt-decode';
+import FacebookLogin from 'react-facebook-login/dist/facebook-login-render-props'
+import { useGoogleLogin  } from '@react-oauth/google';
 
 const LoginSignup = () => {
+
+    const [user, setUser] = useState({});
+    //google login setup
+    const login = useGoogleLogin({
+        onSuccess: codeResponse => {
+            console.log(jwt_decode(codeResponse))
+            setUser(codeResponse)
+        }
+    });
+
+    //facebook login setup
+    const responseFacebook = (response) => {                           //success
+        console.log("facebook result  " + response.credential)
+        setUser(response)
+    }
+    const handleLoginFailure = (error) => {                            //failure
+        console.log(error)
+    }
+
     return (
         <>
-        {/* <img className="tourSet" src={tour_set} alt="tour-set" /> */}
-        <div className="container">
-            <form method="post" className='signupForm'>
-                <div className="header">
-                    <div className="text">Create Account</div>
-                </div>
-                <div className="inputs">
-                    <div className="input">
-                        <input type="text" placeholder='Full Name'/>
+            <img src={logo} alt='logo' className='logo'></img>
+            <img src={tourset} alt='tourset' className='tourset'></img>
+            <div className="container">
+                <form method="post" className='signupForm'>
+                    <div className="header">
+                        <div className="text">Create Account</div>
+                        <div className='customlogin'>
+                            <button type='button' onClick={() => login()} className='custombutton'>
+                            <img src={googlebutton} alt='google'></img>
+                                    <div>
+                                        Signup with Google
+                                    </div>
+                            </button>
+
+                            <div className='loginbutton'>
+                                <FacebookLogin
+                                    appId="3478666502461850"
+                                    autoLoad
+                                    fields="name, email, picture"
+                                    callback={responseFacebook}
+                                    onFailure={handleLoginFailure}
+                                    render={(renderProps) => (
+                                        <button type='button' onClick={renderProps.onClick} className='custombutton'>
+                                            <img src={fb} alt='fb'></img>
+                                            <div>Signup with Facebook</div>
+                                        </button>
+                                    )}
+                                />
+                            </div>
+                        </div>
                     </div>
-                    <div className="input">
-                        <input type="email" placeholder='Email Address'/>
+                    <div className='or'>
+                        -OR-
                     </div>
-                    <div className="input">
-                        <input type="password" placeholder='Password'/>
+                    <div className="inputs">
+                        <div className="input">
+                            <input type="text" placeholder='Full Name' />
+                        </div>
+                        <div className="input">
+                            <input type="email" placeholder='Email Address' />
+                        </div>
+                        <div className="input">
+                            <input type="password" placeholder='Password' />
+                        </div>
+                        <div className="input">
+                            <input type="password" placeholder='Repeat Password' />
+                        </div>
                     </div>
-                    <div className="input">
-                        <input type="password" placeholder='Repear Password'/>
+                    <div className="submit-container">
+                        <button className='signupSubmit' type='submit'>Create Account</button>
                     </div>
-                </div>
-                <div className="submit-container">
-                    <button className='signupSubmit' type='submit'>Create Account</button>
-                </div>
-            </form>
-        </div>
+                    <div className='alreadyacc'>
+                        Already have account?
+                        <a href='./Login.jsx' className='redirect'>Log in</a>
+                    </div>
+                </form>
+
+            </div>
         </>
     )
 }
