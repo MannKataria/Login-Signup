7c0507fc869b216c2de24986914ed433faac1b5b
Mann <mannkataria15@gmail.com>
2023-11-09 23:08:51 +0530

    updated main page

--- src/Components/LoginSignup/Signup.jsx
+++ src/Components/LoginSignup/Signup.jsx
@@ -1,32 +1,8 @@
 import styles from "./Signup.module.css";
 import tour_set from "../Assets/tour-set-signup.svg";
 import logo from "../Assets/logo_signup.svg";
-import { useState } from "react";
-import fb from "../Assets/fb.png";
-import googlebutton from "../Assets/google.png";
-import jwt_decode from 'jwt-decode';
-import FacebookLogin from 'react-facebook-login/dist/facebook-login-render-props'
-import { useGoogleLogin  } from '@react-oauth/google';
 
 const Signup = () => {
-  const [user, setUser] = useState({});
-  //google login setup
-  const login = useGoogleLogin({
-      onSuccess: codeResponse => {
-          console.log(jwt_decode(codeResponse))
-          setUser(codeResponse)
-      }
-  });
-
-  //facebook login setup
-  const responseFacebook = (response) => {                           //success
-      console.log("facebook result  " + response.credential)
-      setUser(response)
-  }
-  const handleLoginFailure = (error) => {                            //failure
-      console.log(error)
-  }
-
   return (
     <>
       <div className={styles.mainCont}>
@@ -38,38 +14,7 @@ const Signup = () => {
           <form method="post" className={styles.signupForm}>
             <div className={styles.header}>
               <div className={styles.text}>Create Account</div>
-              <div className="customlogin">
-                <button
-                  type="button"
-                  onClick={() => login()}
-                  className="custombutton"
-                >
-                  <img src={googlebutton} alt="google"></img>
-                  <div>Signup with Google</div>
-                </button>
-
-                <div className="loginbutton">
-                  <FacebookLogin
-                    appId="3478666502461850"
-                    autoLoad
-                    fields="name, email, picture"
-                    callback={responseFacebook}
-                    onFailure={handleLoginFailure}
-                    render={(renderProps) => (
-                      <button
-                        type="button"
-                        onClick={renderProps.onClick}
-                        className="custombutton"
-                      >
-                        <img src={fb} alt="fb"></img>
-                        <div>Signup with Facebook</div>
-                      </button>
-                    )}
-                  />
-                </div>
-              </div>
             </div>
-            <div className={styles.or}>- OR -</div>
             <div className={styles.inputs}>
               <div className={styles.input}>
                 <input type="text" placeholder="Full Name" />
