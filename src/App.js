a98469f5ac2a177147772ae4d16565e23866d0f9
Mann <mannkataria15@gmail.com>
2023-11-12 00:41:20 +0530

    added home page

--- src/App.js
+++ src/App.js
@@ -2,24 +2,21 @@ import "./App.css";
 import Main from "./Components/Main/Main";
 import Signup from "./Components/LoginSignup/Signup";
 import Login from "./Components/LoginSignup/Login";
-import { useState } from "react";
+import Home from "./Components/Main/Home";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-// import { GoogleOAuthProvider } from "@react-oauth/google";
 
 function App() {
-  const [currPage, setCurrPage] = useState("signup");
   return (
-    // <GoogleOAuthProvider clientId="169559215961-i0o7klhc5rhvsm81nctt95v7drpnenjj.apps.googleusercontent.com">
     <div className="App">
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Main />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
+          <Route path="/home" element={<Home />} />
         </Routes>
       </BrowserRouter>
     </div>
-    // </GoogleOAuthProvider>
   );
 }
 
